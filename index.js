d4544f9d4dc458f0cdb4d171b6f2a6f0a6ee2504
hjwforever <1326153330@qq.com>
2020-12-15 15:20:24 +0800

    update

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ require("./routes/good.routes")(app);
 require("./routes/comment.routes")(app);
 require('./routes/auth.routes')(app);
 require('./routes/user.routes')(app);
-require("./routes/upload.routes")(app);
+// require("./routes/upload.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8085;
